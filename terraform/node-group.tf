8f52b58a6b02f86aacef30a8e6705bcb7890dce8
Naga Teja Reddy <nagatejasannala@gmail.com>
2026-09-08 18:09:48 +0530

    infra: scale EKS node group to three nodes

--- terraform/node-group.tf
+++ terraform/node-group.tf
@@ -13,7 +13,7 @@ resource "aws_eks_node_group" "main" {
   capacity_type = "ON_DEMAND"
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     min_size     = 2
     max_size     = 4
   }
